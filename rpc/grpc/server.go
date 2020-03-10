a098df8d4d5dd99f292343b6ee1ceffb87b67a2c
Lu Rongkai <lurongkai@gmail.com>
2020-03-10 15:12:21 +0800

    nil check

--- rpc/grpc/server.go
+++ rpc/grpc/server.go
@@ -3,12 +3,13 @@ package grpc
 import (
 	"context"
 	"fmt"
-	grpcpkg "google.golang.org/grpc"
+	"time"
+
 	"github.com/sirupsen/logrus"
+	grpcpkg "google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/status"
-	"time"
 )
 
 func NewGrpcServer() *grpcpkg.Server {
@@ -42,22 +43,24 @@ func newErrorHandlingUnaryServerInterceptor() grpcpkg.UnaryServerInterceptor {
 
 		t := time.Now()
 		resp, err = handler(ctx, req)
-		logrus.Infof("%s processed taken %d millis",  info.FullMethod, time.Since(t).Milliseconds())
+		logrus.Infof("%s processed taken %d millis", info.FullMethod, time.Since(t).Milliseconds())
 
-		if _, ok := err.(interface { GRPCStatus() *status.Status }); ok {
-			// if err is GRPC status error object, then do nothing
-			logrus.WithFields(logrus.Fields{"error": err, "type": "grpc-status"}).Debugf("service response status error: %v", err)
-		} else if ex, ok := err.(interface { Business() (uint32, string) }); ok {
-			c, m := ex.Business()
-			logrus.WithFields(logrus.Fields{"error": err, "type": "business"}).Debugf("service response business error: [%d] %s", c, m)
-			err = status.Error(codes.Code(c), m)
-		} else if ex, ok := err.(interface{ Demand() string }); ok {
-			msg := ex.Demand()
-			logrus.WithFields(logrus.Fields{"error": err, "type": "demand"}).Debugf("service response demand error: %s", msg)
-			err = status.Error(codes.InvalidArgument, msg)
-		} else {
-			logrus.WithFields(logrus.Fields{"error": err, "type": "general"}).Debugf("service response general error: %+v", err)
-			err = status.Error(codes.Unknown, err.Error())
+		if err != nil {
+			if _, ok := err.(interface{ GRPCStatus() *status.Status }); ok {
+				// if err is GRPC status error object, then do nothing
+				logrus.WithFields(logrus.Fields{"error": err, "type": "grpc-status"}).Debugf("service response status error: %v", err)
+			} else if ex, ok := err.(interface{ Business() (uint32, string) }); ok {
+				c, m := ex.Business()
+				logrus.WithFields(logrus.Fields{"error": err, "type": "business"}).Debugf("service response business error: [%d] %s", c, m)
+				err = status.Error(codes.Code(c), m)
+			} else if ex, ok := err.(interface{ Demand() string }); ok {
+				msg := ex.Demand()
+				logrus.WithFields(logrus.Fields{"error": err, "type": "demand"}).Debugf("service response demand error: %s", msg)
+				err = status.Error(codes.InvalidArgument, msg)
+			} else {
+				logrus.WithFields(logrus.Fields{"error": err, "type": "general"}).Debugf("service response general error: %+v", err)
+				err = status.Error(codes.Unknown, err.Error())
+			}
 		}
 		return
 	}
