e808a05f86f64b8171445c12820b59378fac6ba5
Lu Rongkai <lurongkai@gmail.com>
2021-03-04 11:28:43 +0800

    support grpc client error msg format

--- rpc/grpc/client.go
+++ rpc/grpc/client.go
@@ -10,11 +10,19 @@ import (
 	"time"
 )
 
+type ErrorFormatter func(code uint32, msg string) string
+
 func Dial(addr string, dialOption grpcpkg.DialOption) (*grpcpkg.ClientConn, error) {
+	return DialWithErrorFormatter(addr, dialOption, func(code uint32, msg string) string {
+		return msg
+	})
+}
+
+func DialWithErrorFormatter(addr string, dialOption grpcpkg.DialOption, errorFormatter ErrorFormatter) (*grpcpkg.ClientConn, error) {
 	return grpcpkg.Dial(
 		addr,
 		dialOption,
-		grpcpkg.WithUnaryInterceptor(newErrorHandlingUnaryClientInterceptor()),
+		grpcpkg.WithUnaryInterceptor(newErrorHandlingUnaryClientInterceptor(errorFormatter)),
 		grpcpkg.WithKeepaliveParams(keepalive.ClientParameters{
 			Time:                10 * time.Minute,
 			Timeout:             5 * time.Second,
@@ -23,7 +31,7 @@ func Dial(addr string, dialOption grpcpkg.DialOption) (*grpcpkg.ClientConn, erro
 	)
 }
 
-func newErrorHandlingUnaryClientInterceptor() grpcpkg.UnaryClientInterceptor {
+func newErrorHandlingUnaryClientInterceptor(errorFormatter ErrorFormatter) grpcpkg.UnaryClientInterceptor {
 	return func(ctx context.Context, method string, req, reply interface{}, cc *grpcpkg.ClientConn, invoker grpcpkg.UnaryInvoker, opts ...grpcpkg.CallOption) error {
 		t := time.Now()
 		err := invoker(ctx, method, req, reply, cc, opts...)
@@ -32,7 +40,7 @@ func newErrorHandlingUnaryClientInterceptor() grpcpkg.UnaryClientInterceptor {
 		if ex := status.Convert(err); ex != nil {
 			code := uint32(ex.Code())
 			if code > uint32(errors.MinimalBusinessCode) {
-				err = errors.NewBusinessError(code, ex.Message())
+				err = errors.NewBusinessError(code, errorFormatter(code, ex.Message()))
 			}
 		}
 
